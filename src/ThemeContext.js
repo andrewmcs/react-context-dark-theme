99059ef61c8bae5cae1f349606a022a50dce1bb5
Andrew M <136806756+andrewmcs@users.noreply.github.com>
2024-11-19 18:52:27 +0000

    completed theme implementation using context

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,31 +1,24 @@
-import { createContext } from "react";
-import { useContext } from "react";
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-/*
-Step 1
-Create a ThemeContext object using React.createContext().
 
+const ThemeContext = createContext(undefined);
+
+/* 
 Implement the ThemeProvider component. 
 It should accept a children prop and return a ThemeContext.Provider component. 
 The ThemeContext.Provider receives an object as its value prop, 
 with a theme string and a toggleTheme function.
 
-toggleTheme should toggle the theme between light and dark.
+ toggleTheme should toggle the theme between light and dark.
 */
-
-
-const ThemeContext = createContext(undefined);
-
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
-
   return (
     <ThemeContext.Provider
       value={
         {
           theme,
-          toggleTheme: setTheme(theme === 'light' ? "dark" : "light")
+          toggleTheme: () => setTheme(theme === "light" ? "dark" : "light")
         }
       }
     >
